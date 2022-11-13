08a090c99c5e077c9b080eebc096fbc516552b1f
melike2d <real.melike2d@gmail.com>
2022-11-12 21:49:44 -0800

    feat: add player events

--- src/player/base.ts
+++ src/player/base.ts
@@ -1,6 +1,21 @@
 import type { StreamableTrack } from "../item/track/streamable";
+import type { PlayerTrack } from "./track/base";
+import type { TypedEmitter } from "tiny-typed-emitter";
+import type { Track } from "../item/track/base";
+import type { PlayerTrackEndReason } from "./track/end_reason";
+import type { FriendlyException } from "../tools/friendly_exception";
 
-export interface Player {
+export interface Player extends TypedEmitter<PlayerEvents> {
+    /**
+     * Whether this player has been paused.
+     */
+    isPaused: boolean;
+
+    /**
+     * The current track being played.
+     */
+    track: PlayerTrack | null;
+
     /**
      * Plays the specified track
      *
@@ -15,4 +30,20 @@ export interface Player {
      * @return `true` if a track was stopped.
      */
     stop(): Promise<boolean>
+}
+
+export interface PlayerEvents {
+    /** Emitted when a track has been started. */
+    trackStart:  (track: PlayerTrack) => void;
+    /** Emitted when the wait threshold was exceeded for this track. */
+    trackStuck:  (track: PlayerTrack, threshold: number) => void;
+    /** Emitted when the current track has ended. */
+    trackEnd:    (track: Track, reason: PlayerTrackEndReason) => void;
+    /** Emitted when an exception occurs in an audio track that causes it to halt. */
+    trackFailed: (track: Track, exception: FriendlyException) => void;
+
+    /** Emitted when playback was paused. */
+    playbackPaused:  (track: PlayerTrack | null) => void;
+    /** Emitted when playback was resumed. */
+    playbackResumed: (track: PlayerTrack | null) => void;
 }
